20919b983b0a3754774844f39c15379573bf0c54
John Grasty <john@needfaith.org>
2015-07-16 23:43:16 +0200

    Typo

--- copy/var/zoneinit/includes/25-zabbix-proxy.sh
+++ copy/var/zoneinit/includes/25-zabbix-proxy.sh
@@ -9,7 +9,7 @@ if mdata-get zabbix_server 1>/dev/null 2>&1; then
 	echo "Server=$(mdata-get zabbix_server)" > ${ZABBIX_PROXY_CONFD}/server.conf
 	
 	cp /opt/core/var/zabbix/proxy.conf ${ZABBIX_PROXY_CONFD}/
-	mv /opt/core/var/zabbix/zabbix-proxy.con ${ZABBIX_PROXY_CONF}
+	mv /opt/core/var/zabbix/zabbix-proxy.conf ${ZABBIX_PROXY_CONF}
 	
 	svcadm enable zabbix-proxy
 	
